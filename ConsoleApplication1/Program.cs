7f49e28fd5223db6229077b86292185979cfcda4
Nagaraj <Samarth.Nagaraj@education.vic.gov.au>
2020-03-18 14:31:32 +1100

    console command added

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Some title for testing");
+            Console.Read();
         }
     }
 }
